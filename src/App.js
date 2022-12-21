a7060a594a373bbaa914a4c9e0f2c27a96a5622c
Aleena Irfan <aleenaazqa8@gmail.com>
2022-12-21 15:43:12 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Hellow from './hellow'
 
 function App({name, age}) {
   return <div>Hellow world from App.js {name} Age = {age - 10}
       <div>
-        Another tree
-      </div>
+        Another tree div inside div
+        </div>
+        <br/>
+      <Hellow firstName={name}></Hellow>
     </div>
   
   }
---
+++ src/hellow.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './hellow.css'
+
+function Hellow({firstName}) {
+  return <p class="myname">This is para in hellow tag name : {firstName}</p>
+  
+  }
+
+export default Hellow;
